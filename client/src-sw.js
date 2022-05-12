c6eb39d51ab03b935f05ba8a474f52bdabf15d4e
jacob ramirez <jacobnramirez@gmail.com>
2022-05-12 10:01:44 -0700

    editing sw.js to try and fix offline/getall function

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,18 +1,30 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate  } = require('workbox-strategies');
+const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-import { setDefaultHandler } from 'workbox-routing';
-import { NetworkOnly } from 'workbox-strategies';
+// import { setDefaultHandler } from 'workbox-routing';
+// import { NetworkOnly } from 'workbox-strategies';
 
-setDefaultHandler(new NetworkOnly());
+// setDefaultHandler(new NetworkOnly());
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+const assetCache = new CacheFirst({
+  cacheName: 'assets',
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -29,21 +41,7 @@ warmStrategyCache({
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    // Name of the cache storage.
-    cacheName: 'asset-cache',
-    plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
-);
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => request.mode === 'image', assetCache);
 
-offlineFallback();
+// offlineFallback();
